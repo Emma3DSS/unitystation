bc65b361fc9351de154520357d409d82fcbdc2e5
Emma3DSS <ruben9676@outlook.com>
2020-12-04 20:52:25 +1000

    Guns and Drink Containers

--- UnityProject/Assets/Scripts/Items/Weapons/Gun.cs
+++ UnityProject/Assets/Scripts/Items/Weapons/Gun.cs
@@ -102,7 +102,7 @@ namespace Weapons
 		/// <summary>
 		/// The name of the sound this gun makes when shooting
 		/// </summary>
-		[FormerlySerializedAs("FireingSound"), Tooltip("The name of the sound the gun uses when shooting (must be in soundmanager")]
+		[Tooltip("The name of the sound the gun uses when shooting (must be in soundmanager")]
 		public string FiringSound;
 
 
@@ -110,7 +110,7 @@ namespace Weapons
 		/// The name of the sound this gun makes when shooting
 		/// </summary>
 		[Tooltip("The name of the sound the gun uses when shooting (must be in soundmanager")]
-		public AddressableAudioSource FiringSoundA;
+		[SerializeField] private AddressableAudioSource FiringSoundA = null;
 		//RRTL
 
 		/// <summary>
@@ -123,7 +123,7 @@ namespace Weapons
 		/// The name of the sound this gun makes when shooting
 		/// </summary>
 		[Tooltip("The name of the sound the gun uses when shooting with a suppressor attached (must be in soundmanager")]
-		public AddressableAudioSource SuppressedSoundA;
+		[SerializeField] private AddressableAudioSource SuppressedSoundA = null;
 		//RRTL
 
 		/// <summary>
